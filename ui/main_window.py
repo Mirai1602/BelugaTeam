cd93a2a1dbf56ad8806634559ca7e59ca51502c1
Fabiola Saraí Lanuza Paz <fslanuza@uamv.edu.ni>
2026-09-22 03:56:58 -0600

    feat(ui): agregar HomeWindow y MainWindow para la interfaz principal

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,68 +1,74 @@
-from PyQt6.QtWidgets import (
-    QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
-)
+from qfluentwidgets import FluentWindow, FluentIcon as FIF
 
-from ui.vectoriales_window import VectorialesWindow
-from ui.ecuaciones_window import EcuacionesWindow
+from ui.home_window import HomeWindow
 from ui.basica_window import CalculadoraMatrices
 from ui.avanzada_window import CalculadoraMatricesAvanzada
+from ui.vectoriales_window import VectorialesWindow
+from ui.ecuaciones_window import EcuacionesWindow
 
 
-class MainWindow(QWidget):
+class MainWindow(FluentWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("BelugaCalc")
-        self.resize(700, 450)
+        self.resize(1100, 750)
 
-        layout_principal = QVBoxLayout()
+        # Pantallakuna
+        self.home_window = HomeWindow(self)
+        self.ventana_basica = CalculadoraMatrices(self)
+        self.ventana_avanzada = CalculadoraMatricesAvanzada(self)
+        self.ventana_vectoriales = VectorialesWindow(self)
+        self.ventana_ecuaciones = EcuacionesWindow(self)
 
-        titulo = QLabel("<h1>BelugaCalc</h1>")
-        subtitulo = QLabel(
-            "Bienvenido/a a BelugaCalc, una calculadora virtual para matrices!"
+        # Sutinkuna navegacionpaq
+        self.home_window.setObjectName("homeWindow")
+        self.ventana_basica.setObjectName("basicaWindow")
+        self.ventana_avanzada.setObjectName("avanzadaWindow")
+        self.ventana_vectoriales.setObjectName("vectorialesWindow")
+        self.ventana_ecuaciones.setObjectName("ecuacionesWindow")
+
+        self.init_navigation()
+
+    def init_navigation(self):
+        # Qallariy (Inicio)
+        self.addSubInterface(
+            self.home_window,
+            icon=FIF.HOME,
+            text="Inicio"
         )
 
-        layout_principal.addWidget(titulo)
-        layout_principal.addWidget(subtitulo)
+        # Módulos (Iconokuna allinchasqa)
+        self.addSubInterface(
+            self.ventana_basica,
+            icon=FIF.APPLICATION,
+            text="Básica"
+        )
+        self.addSubInterface(
+            self.ventana_avanzada,
+            icon=FIF.EDIT,
+            text="Avanzada"
+        )
+        self.addSubInterface(
+            self.ventana_vectoriales,
+            icon=FIF.CODE,
+            text="Vectores"
+        )
+        self.addSubInterface(
+            self.ventana_ecuaciones,
+            icon=FIF.DOCUMENT,
+            text="Ecuaciones Matriciales"
+        )
 
-        botones_layout = QHBoxLayout()
-
-        btn_basica = QPushButton("Básica")
-        btn_avanzada = QPushButton("Avanzada")
-
-        btn_basica.clicked.connect(self.abrir_basica)
-        btn_avanzada.clicked.connect(self.abrir_avanzada)
-
-        botones_layout.addWidget(btn_basica)
-        botones_layout.addWidget(btn_avanzada)
-        btn_vectoriales = QPushButton("Vectores")
-        btn_vectoriales.clicked.connect(self.abrir_vectoriales)
-        botones_layout.addWidget(btn_vectoriales)
-
-        btn_ecuaciones = QPushButton("Ecuaciones matriciales")
-        btn_ecuaciones.clicked.connect(self.abrir_ecuaciones)
-        botones_layout.addWidget(btn_ecuaciones)
-
-        layout_principal.addLayout(botones_layout)
-
-        self.setLayout(layout_principal)
-
-    def abrir_basica(self):
-        self.ventana_basica = CalculadoraMatrices(self)
-        self.hide()
-        self.ventana_basica.show()
-
-    def abrir_avanzada(self):
-        self.ventana_avanzada = CalculadoraMatricesAvanzada(self)
-        self.hide()
-        self.ventana_avanzada.show()
-
-    def abrir_vectoriales(self):
-        self.ventana_vectoriales = VectorialesWindow(self)
-        self.hide()
-        self.ventana_vectoriales.show()
-
-    def abrir_ecuaciones(self):
-        self.ventana_ecuaciones = EcuacionesWindow(self)
-        self.hide()
-        self.ventana_ecuaciones.show()
+    def navegar_a(self, clave):
+        mapa_modulos = {
+            "home": self.home_window,
+            "basica": self.ventana_basica,
+            "avanzada": self.ventana_avanzada,
+            "vectores": self.ventana_vectoriales,
+            "ecuaciones": self.ventana_ecuaciones
+        }
+        
+        target = mapa_modulos.get(clave)
+        if target:
+            self.switchTo(target)
